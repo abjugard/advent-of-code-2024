6b4c5286a7d43a671470dfe783d67af49413bff5
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2024-12-02 06:55:24 +0100

    Day 02: Red-Nosed Reports

---
+++ src/day02-red_nosed_reports.py
@@ -0,0 +1,36 @@
+from santas_little_helpers import day, get_data, timed
+from itertools import pairwise
+
+today = day(2024, 2)
+
+
+def gradual(levels):
+  return all(1 <= abs(r-l) <= 3 for l, r in pairwise(levels))
+
+
+def unidirectional(levels):
+  sorted_levels = sorted(levels)
+  return levels == sorted_levels or levels[::-1] == sorted_levels
+
+
+def safe(levels, use_problem_dampener=False):
+  if not use_problem_dampener:
+    return unidirectional(levels) and gradual(levels)
+  for i in range(len(levels)):
+    if safe(levels[:i] + levels[i+1:]):
+      return True
+  return False
+
+
+def parse(line):
+  return list(map(int, line.split()))
+
+
+def main():
+  reports = list(get_data(today, [('func', parse)]))
+  print(f'{today} star 1 = {sum(safe(report) for report in reports)}')
+  print(f'{today} star 2 = {sum(safe(report, use_problem_dampener=True) for report in reports)}')
+
+
+if __name__ == '__main__':
+  timed(main)
