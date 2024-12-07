6936e0498547a45832b6a69484755762713c7b40
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2024-12-07 22:52:57 +0100

    Day 07: Faster and cleaned up

--- src/day07-bridge_repair.py
+++ src/day07-bridge_repair.py
@@ -3,31 +3,30 @@ from santas_little_helpers import day, get_data, timed
 today = day(2024, 7)
 
 
-def test_eqn(test_value, ns, concat = False):
-  if len(ns) == 1:
-    return test_value == ns[0]
-  l, r, *rest = ns
-  if test_eqn(test_value, [l + r] + rest, concat):
-    return test_value
-  if test_eqn(test_value, [l * r] + rest, concat):
-    return test_value
-  if concat:
-    head = int(str(l) + str(r))
-    if test_eqn(test_value, [head] + rest, concat):
-      return test_value
-  return 0
+def test_equations(equations, concat = False):
+  def test_eqn(test_value, eqn):
+    def dfs(rest):
+      if len(rest) == 1:
+        return test_value == rest[0]
+      n1, n2, *tail = rest
+      if n1 > test_value:
+        return False
+      if dfs([n1 * n2] + tail) or dfs([n1 + n2] + tail):
+        return True
+      return concat and dfs([int(f'{n1}{n2}')] + tail)
+    return test_value * dfs(eqn)
+  return sum(test_eqn(*eqn) for eqn in equations)
 
 
 def parse(line):
-  l, raw = line.split(':')
-  r = raw.split()
-  return int(l.strip()), list(map(int, r))
+  test_value, eqn = line.split(':')
+  return int(test_value), list(map(int, eqn.split()))
 
 
 def main():
-  equations = list(get_data(today, [('func', parse)]))
-  print(f'{today} star 1 = {sum(test_eqn(*eqn) for eqn in equations)}')
-  print(f'{today} star 2 = {sum(test_eqn(*eqn, concat=True) for eqn in equations)}')
+  equations = list(get_data(today, parse))
+  print(f'{today} star 1 = {test_equations(equations)}')
+  print(f'{today} star 2 = {test_equations(equations, concat = True)}')
 
 
 if __name__ == '__main__':
