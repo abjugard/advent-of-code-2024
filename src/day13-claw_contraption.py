428cc6f94d7b5f99a1eeb295d561b086e98e1cc3
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2024-12-15 20:18:52 +0100

    Day 13: Cleaner

--- src/day13-claw_contraption.py
+++ src/day13-claw_contraption.py
@@ -4,13 +4,13 @@ from sympy import solve, symbols, Integer
 today = day(2024, 13)
 
 
-def win_machine(a_inp, b_inp, prize, inc_prize: float=0):
-  (ax, ay), (bx, by), (px, py) = a_inp, b_inp, [int(p + inc_prize) for p in prize]
+def win_machine(a_inp, b_inp, prize, inc_prize=0):
+  (ax, ay), (bx, by), (px, py) = a_inp, b_inp, prize
 
   a, b = symbols('a b')
   a, b = result = solve([
-    ax*a + bx*b - px,
-    ay*a + by*b - py
+    ax*a + bx*b - px - inc_prize,
+    ay*a + by*b - py - inc_prize
   ]).values()
 
   return 3*a + b if all(isinstance(n, Integer) for n in result) else 0
@@ -27,7 +27,7 @@ def parse(group):
 def main():
   machines = list(map(parse, get_data(today, groups=True)))
   print(f'{today} star 1 = {sum(win_machine(*m) for m in machines)}')
-  print(f'{today} star 2 = {sum(win_machine(*m, inc_prize=1e13) for m in machines)}')
+  print(f'{today} star 2 = {sum(win_machine(*m, inc_prize=int(1e13)) for m in machines)}')
 
 
 if __name__ == '__main__':
