5c663e9588c8138c75d960fcd6b6a5a6eaa4b6c5
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2024-12-18 09:52:40 +0100

    Day 18: 🏎️💨 Sanic Fast

--- src/day18-ram_run.py
+++ src/day18-ram_run.py
@@ -4,10 +4,6 @@ from santas_little_utils import neighbours, ints, all_points
 
 today = day(2024, 18)
 
-w = h = 70
-start, end = (0, 0), (w, h)
-base_points = set(all_points(w+1, h+1))
-
 
 def find_shortest_exit(traversable):
   g = nx.Graph()
@@ -15,24 +11,28 @@ def find_shortest_exit(traversable):
     g.add_node(p)
     for n in neighbours(p, borders=traversable):
       g.add_edge(p, n)
-  if nx.has_path(g, start, end):
-    return nx.shortest_path_length(g, start, end)
+  if nx.has_path(g, (0, 0), (70, 70)):
+    return set(nx.shortest_path(g, (0, 0), (70, 70)))
   return None
 
 
-def find_first_blocking(falling_bytes):
+def find_first_blocking(traversable, falling_bytes):
   last_blocking = None
-  for l in reversed(range(len(falling_bytes))):
-    if not find_shortest_exit(base_points - set(falling_bytes[:l + 1])):
-      last_blocking = falling_bytes[l]
-    else:
-      return ','.join(map(str, last_blocking))
+  while path := find_shortest_exit(traversable):
+    while falling_bytes:
+      last_blocking = falling_bytes.pop(0)
+      traversable.remove(last_blocking)
+      if last_blocking in path:
+        break
+  return ','.join(map(str, last_blocking))
 
 
 def main():
   falling_bytes = list(get_data(today, [('func', (ints, ','))], groups=False))
-  print(f'{today} star 1 = {find_shortest_exit(base_points - set(falling_bytes[:1024]))}')
-  print(f'{today} star 2 = {find_first_blocking(falling_bytes)}')
+  fallen, falling = falling_bytes[:1024], falling_bytes[1024:]
+  base_points = set(all_points(71, 71)) - set(fallen)
+  print(f'{today} star 1 = {len(find_shortest_exit(base_points))-1}')
+  print(f'{today} star 2 = {find_first_blocking(base_points, falling)}')
 
 
 if __name__ == '__main__':
