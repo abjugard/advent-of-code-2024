d4dc7847568e533b59cde205cb1fcaa991c4eebf
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2024-12-19 10:05:34 +0100

    Day 19: Prettier

--- src/day19-linen_layout.py
+++ src/day19-linen_layout.py
@@ -2,20 +2,7 @@ from functools import cache
 from santas_little_helpers import day, get_data, timed
 
 today = day(2024, 19)
-
-
 towels = []
-
-
-@cache
-def is_possible(pattern):
-  if len(pattern) == 0:
-    return True
-  return any(
-    is_possible(pattern[len(towel):])
-    for towel in towels
-    if pattern.startswith(towel)
-  )
 
 
 @cache
@@ -30,12 +17,12 @@ def count_ways(pattern):
 
 
 def main():
-  global towels
-  towels, patterns = list(get_data(today, groups=True))
-  patterns = list(patterns)
-  towels = list(next(towels).split(', '))
-  print(f'{today} star 1 = {sum(is_possible(p) for p in patterns)}')
-  print(f'{today} star 2 = {sum(count_ways(p) for p in patterns)}')
+  towels_raw, patterns = get_data(today, groups=True)
+  towels.extend(next(towels_raw).split(', '))
+
+  ways = list(filter(None, (count_ways(pattern) for pattern in patterns)))
+  print(f'{today} star 1 = {len(ways)}')
+  print(f'{today} star 2 = {sum(ways)}')
 
 
 if __name__ == '__main__':
