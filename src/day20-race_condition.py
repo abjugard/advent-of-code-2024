f1d23efc48c4b1f2c278d0eb15c4b853579a6ea6
Adrian Bjugård <287697+abjugard@users.noreply.github.com>
2024-12-20 20:33:56 +0100

    Day 20: Improve readability

--- src/day20-race_condition.py
+++ src/day20-race_condition.py
@@ -9,16 +9,12 @@ def build_graph(the_map, d, noclip=False):
   g = nx.Graph()
   start = end = None
   for p, c in the_map.items():
-    if c == '#' and not noclip:
-      continue
-    if c == 'S':
-      start = p
-    if c == 'E':
-      end = p
-    for n in neighbours(p, borders=(d if noclip else the_map)):
-      if the_map[n] == '#' and not noclip:
-        continue
-      g.add_edge(p, n)
+    if c == 'S': start = p
+    if c == 'E': end = p
+    if c != '#' or noclip:
+      for n in neighbours(p, borders=(d if noclip else the_map)):
+        if the_map[n] != '#' or noclip:
+          g.add_edge(p, n)
   return g, start, end
 
 
@@ -27,17 +23,15 @@ def find_cheats(data, noclip_distance=2):
   g_all, *_ = build_graph(*data, noclip=True)
 
   time_to_beat = nx.shortest_path_length(g, start, end) - 100
-  from_start = nx.shortest_path_length(g, start)
-  to_end = nx.shortest_path_length(g, end)
+  from_start   = nx.shortest_path_length(g, start)
+  to_end       = nx.shortest_path_length(g, end)
 
   count = 0
-  for c_s in g.nodes():
-    if c_s == end:
-      continue
-    cheats = nx.single_source_shortest_path_length(g_all, c_s, cutoff=noclip_distance)
-    for c_e, clip_length in cheats.items():
-      if c_e in g.nodes():
-        count += from_start[c_s] + clip_length + to_end[c_e] <= time_to_beat
+  for c_start in g.nodes():
+    cheats = nx.single_source_shortest_path_length(g_all, c_start, cutoff=noclip_distance)
+    for c_end, clip_length in cheats.items():
+      if c_end in g.nodes():
+        count += from_start[c_start] + clip_length + to_end[c_end] <= time_to_beat
   return count
 
 
